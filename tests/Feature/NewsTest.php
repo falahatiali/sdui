97c86456ec86bc9a1b65b863755a46b347e72ebe
Ali Agho <ali_fm2009@yahoo.com>
2022-12-15 10:08:20 +0100

    add tests

--- tests/Feature/NewsTest.php
+++ tests/Feature/NewsTest.php
@@ -3,8 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\News;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
 use Tests\TestCase;
 
 class NewsTest extends TestCase
@@ -36,23 +35,136 @@ class NewsTest extends TestCase
         ]
     ];
 
+    private User $user;
+
+    private array $commonHeaders;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+
+        $this->commonHeaders = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ];
+    }
 
     /** @test */
     public function test_index(): void
     {
         News::factory(10)->create();
 
-        $this->get('api/news')
+        $this->get('api/news', $this->commonHeaders)
             ->assertStatus(200)
             ->assertJsonStructure(self::JSON_COLLECTION_STRUCTURE);
     }
 
     /** @test */
-    public function test_show_api()
+    public function test_show_api_that_requires_authentication_token()
     {
         $news = News::factory()->create();
 
-        $this->get('/api/news/' . $news->id)
+        $this->get('/api/news/' . $news->id, $this->commonHeaders)
+            ->assertStatus(401);
+    }
+
+    /** @test */
+    public function test_show_api_that_user_can_see_a_news()
+    {
+        $news = News::factory()->create();
+        $this->actingAs($this->user);
+
+        $this->get('/api/news/' . $news->id, $this->commonHeaders)
             ->assertStatus(200);
     }
+
+    /** @test */
+    public function test_news_store_api_validate_title_that_its_required()
+    {
+        $params = [
+            'content' => fake()->sentence(12)
+        ];
+        $this->actingAs($this->user);
+
+        $this->postJson('/api/news', $params, $this->commonHeaders)
+            ->assertStatus(422)
+            ->assertSeeText('The title field is required.');
+
+    }
+
+    /** @test */
+    public function test_news_store_api_validate_title_that_it_should_have_at_least_10_words()
+    {
+        $params = [
+            'title' => 'Hi',
+            'content' => fake()->sentence(12)
+        ];
+        $this->actingAs($this->user);
+
+        $this->postJson('/api/news', $params, $this->commonHeaders)
+            ->assertStatus(422)
+            ->assertSeeText('The title must be at least 10 characters.');
+    }
+
+    /** @test */
+    public function test_news_store_api_validate_title_that_its_max_words_is_200()
+    {
+        $params = [
+            'title' => fake()->sentence(201),
+            'content' => fake()->sentence(12)
+        ];
+        $this->actingAs($this->user);
+
+        $this->postJson('/api/news', $params, $this->commonHeaders)
+            ->assertStatus(422)
+            ->assertSeeText('The title must not be greater than 200 characters.');
+    }
+
+    /** @test */
+    public function test_news_store_api_validate_content_that_its_required()
+    {
+        $params = [
+            'title' => fake()->sentence(6),
+        ];
+
+        $this->actingAs($this->user);
+
+        $this->postJson('/api/news', $params, $this->commonHeaders)
+            ->assertStatus(422)
+            ->assertSeeText('The content field is required.');
+    }
+
+    /** @test */
+    public function test_news_store_api_validate_content_that_it_should_have_at_least_10_words()
+    {
+        $params = [
+            'title' => fake()->sentence(12),
+            'content' => 'Hi there'
+        ];
+        $this->actingAs($this->user);
+
+        $this->postJson('/api/news', $params, $this->commonHeaders)
+            ->assertStatus(422)
+            ->assertSeeText('The content must be at least 10 characters.');
+    }
+
+    /** @test */
+    public function test_create_news()
+    {
+        $params = [
+            'title' => fake()->sentence(15),
+            'content' => fake()->sentence(32)
+        ];
+        $this->actingAs($this->user);
+
+        $this->postJson('/api/news', $params, $this->commonHeaders)
+            ->assertStatus(201);
+
+        self::assertDatabaseHas('news', $params);
+        self::assertDatabaseCount('news', 1);
+    }
+
+
 }
--- tests/Unit/Models/NewsTest.php
+++ tests/Unit/Models/NewsTest.php
@@ -2,17 +2,50 @@
 
 namespace Tests\Unit\Models;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\News;
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
 
-class News extends TestCase
+class NewsTest extends TestCase
 {
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_schema_has_predefined_columns(): void
     {
-        $this->assertTrue(true);
+        $this->assertEquals(
+            1, Schema::hasColumns('news',
+            ['id', 'title', 'content', 'created_at', 'updated_at'])
+        );
+    }
+
+    /** @test  */
+    public function test_a_user_has_many_news()
+    {
+        /** @var User $user */
+        $user = User::factory()->create();
+
+        News::factory(3, [
+            'user_id' => $user->id
+        ])->create();
+
+        $this->assertEquals(3, $user->news()->count());
+        $this->assertInstanceOf(News::class, $user->news->first());
+    }
+
+    /** @test  */
+    public function test_a_news_belongs_to_user()
+    {
+        /** @var User $user */
+        $user = User::factory()->create();
+
+        $news = News::factory([
+            'user_id' => $user->id
+        ])->create();
+
+        $this->assertInstanceOf(User::class, $news->user);
     }
 }
